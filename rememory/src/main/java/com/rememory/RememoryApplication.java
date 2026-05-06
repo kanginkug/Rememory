26da858536ec2e622454f457d463986d94e5095d
강인국 <p2t2r1234@gmail.com>
2026-05-07 07:50:44 +0900

    첫 프로젝트 연결

---
+++ rememory/src/main/java/com/rememory/RememoryApplication.java
@@ -0,0 +1,13 @@
+package com.rememory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememoryApplication.class, args);
+	}
+
+}
---
+++ rememory/src/test/java/com/rememory/RememoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rememory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
