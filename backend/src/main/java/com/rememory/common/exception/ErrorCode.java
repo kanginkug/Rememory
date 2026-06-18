79e421eaf53512a5e5e20eda739990fbfd318e49
강인국 <p2t2r1234@gmail.com>
2026-06-18 11:36:16 +0900

    에러코드 메세지 수정
    - 메모리 -> 추억
    - 리뷰 -> 후기

--- backend/src/main/java/com/rememory/common/exception/ErrorCode.java
+++ backend/src/main/java/com/rememory/common/exception/ErrorCode.java
@@ -13,16 +13,16 @@ public enum ErrorCode {
     MEMBER_ALREADY_DELETED("M002", "이미 탈퇴한 회원입니다.", HttpStatus.FORBIDDEN),
 
     // ===== Memory =====
-    MEMORY_NOT_FOUND("MM001", "존재하지 않는 메모리입니다.", HttpStatus.BAD_REQUEST),
+    MEMORY_NOT_FOUND("MM001", "존재하지 않는 추억입니다.", HttpStatus.BAD_REQUEST),
     MEMORY_ACCESS_DENIED("MM002", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
-    MEMORY_NOT_CREATOR("MM003", "메모리 생성자만 가능합니다.", HttpStatus.FORBIDDEN),
-    MEMORY_PHOTO_NOT_FOUND("MM004", "메모리 사진이 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
+    MEMORY_NOT_CREATOR("MM003", "추억 생성자만 가능합니다.", HttpStatus.FORBIDDEN),
+    MEMORY_PHOTO_NOT_FOUND("MM004", "추억 사진이 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
     MEMORY_PHOTO_ALREADY_EXISTS("MM005", "이미 표지 사진이 존재합니다.", HttpStatus.CONFLICT),
     MEMORY_HAS_PLACES("MM006", "장소가 존재하기 때문에 추억을 삭제할 수 없습니다.", HttpStatus.CONFLICT),
 
     // ===== MemberMemory =====
-    MEMBER_MEMORY_ALREADY_EXISTS("MME001", "이미 참가한 메모리입니다.", HttpStatus.CONFLICT),
-    MEMBER_MEMORY_NOT_FOUND("MME002", "메모리 멤버가 아닙니다.", HttpStatus.FORBIDDEN),
+    MEMBER_MEMORY_ALREADY_EXISTS("MME001", "이미 참가한 추억입니다.", HttpStatus.CONFLICT),
+    MEMBER_MEMORY_NOT_FOUND("MME002", "추억 멤버가 아닙니다.", HttpStatus.FORBIDDEN),
 
     // ===== Invitation =====
     INVITATION_NOT_FOUND("I001", "존재하지 않는 초대 링크입니다.", HttpStatus.BAD_REQUEST),
@@ -40,8 +40,8 @@ public enum ErrorCode {
     REVIEW_NOT_FOUND("R001", "존재하지 않는 후기입니다.", HttpStatus.BAD_REQUEST),
     REVIEW_ALREADY_EXISTS("R002", "이미 후기를 작성했습니다.", HttpStatus.CONFLICT),
     REVIEW_NOT_OWNER("R003", "후기 작성자만 가능합니다.", HttpStatus.FORBIDDEN),
-    REVIEW_PHOTO_MAX_COUNT("R004", "리뷰 이미지는 3장까지만 저장 가능합니다.", HttpStatus.CONFLICT),
-    REVIEW_PHOTO_ACCESS_DENIED("R005", "해당 리뷰 사진에 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    REVIEW_PHOTO_MAX_COUNT("R004", "후기 이미지는 3장까지만 저장 가능합니다.", HttpStatus.CONFLICT),
+    REVIEW_PHOTO_ACCESS_DENIED("R005", "해당 후기 사진에 접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
 
     // ===== Photo =====
     INVALID_FILE_TYPE("PT001", "이미지 파일만 업로드할 수 있습니다.", HttpStatus.BAD_REQUEST),
