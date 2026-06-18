bf309623b0acfca6f7135a76560396405d995662
강인국 <p2t2r1234@gmail.com>
2026-06-18 12:34:51 +0900

    주석 타입 수정

--- backend/src/main/java/com/rememory/place/PlaceController.java
+++ backend/src/main/java/com/rememory/place/PlaceController.java
@@ -16,7 +16,7 @@ public class PlaceController {
     private final PlaceService placeService;
     private final KakaoPlaceSearchService kakaoPlaceSearchService;
 
-    // 장소 등록 (사진 선택)
+    /** 장소 등록 (사진 선택) */
     @PostMapping(value = "/{memoryId}")
     public ResponseEntity<Void> createPlace(@RequestAttribute("memberId") Long memberId, @PathVariable("memoryId") Long memoryId, @RequestBody @Valid CreatePlaceRequestDTO cpRequestDTO) {
         placeService.save(memoryId, memberId, cpRequestDTO);
